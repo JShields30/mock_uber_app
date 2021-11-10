72ae16d87d7b0b3326bc21685849c75ff2e61f22
Joe Shields <shields.john.joseph@gmail.com>
2021-11-10 14:27:52 -0600

    Uber Logo created

--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import tw from 'tailwind-styled-components'
-import mapboxgl from 'mapbox-gl'
-
-
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9lczMxMiIsImEiOiJja3ZsbzhmaGVibGZiMnVvZjd2dHJwbXI4In0.LyMRdJiMExDGijeUTvDQmA';
-
+import Map from './components/Map'
 
 export default function Home() {
-  useEffect(() => {
-   const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-87.8, 42.2],
-      zoom: 11,
-    });
-  });
 
-  // const map = new mapbpxgl.Map({
-  //   container: 'map',
-  //   style: 'mapbox://styles/',
-  //   center: ,
-  //   zoom: ,
-  // })
- 
   return (
     <Wrapper>
-       <Map id = 'map'></Map>
-       <ActionItems>Start</ActionItems>
+       <Map />
+       <ActionItems>
+         <Header><UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg" /> 
+         
+         Profile</Header>
+         
+       </ActionItems>
     </Wrapper>
   )
 }
@@ -37,10 +23,12 @@ export default function Home() {
 const Wrapper = tw.div`
  flex flex-col bg-red-300 h-screen
 `
-const Map = tw.div`
-bg-red-500 flex-1
-`
-
 const ActionItems = tw.div`
  flex-1
+`
+const Header = tw.div`
+
+`
+const UberLogo = tw.img`
+
 `
