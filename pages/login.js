3809846917399aaaa4590b6321749be0bfeebb71
Joe Shields <shields.john.joseph@gmail.com>
2021-11-16 17:41:39 -0600

    created title and head image components in login page

--- pages/login.js
+++ pages/login.js
@@ -6,6 +6,8 @@ const Login = () => {
     return (
         <Wrapper>
             <UberLogo src="https://i.ibb.co/ZMhy8ws/uber-logo.png" />
+            <HeadImage src="https://i.ibb.co/CsV9RYZ/login-image.png" />
+            <Title>Log in to access your account.</Title>
             <SignInButton>Sign in with Google</SignInButton>
         </Wrapper>
     )
@@ -20,5 +22,11 @@ const SignInButton = tw.button`
 bg-black text-white text-center py-4 mt-8 self-center w-full cursor-pointer
 `
 const UberLogo = tw.img`
+h-20 w-auto object-contain self-start
+`
+const Title = tw.div`
+text-5xl pt-4 text-gray-500
+`
+const HeadImage = tw.img`
 
 `
