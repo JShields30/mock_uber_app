cbacd12979301852ed80e684e445238f6523a66d
Joe Shields <shields.john.joseph@gmail.com>
2021-11-16 17:37:19 -0600

    created sign in button and uber logo components on login page

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import tw from 'tailwind-styled-components';
+import Link from 'next/link';
 
 const Login = () => {
     return (
         <Wrapper>
-            
+            <UberLogo src="https://i.ibb.co/ZMhy8ws/uber-logo.png" />
+            <SignInButton>Sign in with Google</SignInButton>
         </Wrapper>
     )
 }
@@ -11,5 +14,11 @@ const Login = () => {
 export default Login
 
 const Wrapper = tw.div`
+flex flex-col h-screen bg-gray-200 w-screen p-4
+`
+const SignInButton = tw.button`
+bg-black text-white text-center py-4 mt-8 self-center w-full cursor-pointer
+`
+const UberLogo = tw.img`
 
 `
