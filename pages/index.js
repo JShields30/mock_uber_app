295b443a569840b31b4b439ac264fa2e2c30394e
Joe Shields <shields.john.joseph@gmail.com>
2021-11-17 19:28:08 -0600

    pulled google credentials from users to display name and google image

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import tw from 'tailwind-styled-components';
 import Map from './components/Map';
 import Link from 'next/link'
+import { auth } from '../firebase';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+
+	const [user, setUser] = useState(null)
+
+	const router = useRouter()
+
+	useEffect(() => {
+		return onAuthStateChanged(auth, user => {
+			if(user) {
+				setUser({
+					name: user.displayName,
+					photoUrl: user.photoURL,
+				})
+			} else {
+				setUser(null)
+				router.push('./login')
+			}
+
+		})
+
+	}, [])
+
 	return (
 		<Wrapper>
 			<Map />
@@ -14,8 +38,8 @@ export default function Home() {
 				<Header>
 					<UberLogo src='https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg' />
 					<Profile>
-						<Name>Joe Shields</Name>
-						<UserImage src='https://drive.google.com/uc?id=1sQIuxYprbofLN__tpEBbR7urzJFVBRj6' />
+						<Name>{user && user.name}</Name>
+						<UserImage src={user && user.photoUrl} />
 					</Profile>
 				</Header>
 				<ActionButtons>
@@ -50,7 +74,7 @@ const Name = tw.div`
 mr-4 w-20 text-sm
 `;
 const UserImage = tw.img`
-h-16 w-16 rounded-full border border-gray-200 p-px
+h-14 w-14 rounded-full border border-gray-200 p-px
 `;
 const ActionButtons = tw.div`
 flex
